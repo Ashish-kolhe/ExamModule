cf9be8dbf1b383e18a0f53f35f6475a657653a0c
vishal vadlik <vishalvadlik@email.com>
2026-01-21 12:51:51 +0530

    card done

--- src/admin/components/Card.jsx
+++ src/admin/components/Card.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 
+
 export const Card = () => {
   return (
     <div className="card">
-      Card / Menu Area
+
+      <button className="overview-btn">
+        <i className="fa-solid fa-chart-line"></i>
+        Overview
+      </button>
+      <button className="overview-btn">
+        <i className="fa-solid fa-pen-to-square"></i>
+        Exam
+      </button>
+      <button className="overview-btn">
+        <i className="fa-solid fa-trophy result-icon"></i>
+        Results
+      </button><br></br>
+      <hr />
+      <br></br>
+      <button className="overview-btn">
+        <i className="fa-solid fa-chart-line analytics-icon"></i>
+        Analytics
+      </button>
+      <button className="overview-btn">
+        <i className="fa-solid fa-user-graduate student-icon"></i>
+        Student
+      </button>
+
+
+
+
+
+
+
     </div>
   )
 }
