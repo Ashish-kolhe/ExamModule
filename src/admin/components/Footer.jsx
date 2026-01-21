d38eb2a71420889e8b00032b749d81919c1731e7
OnkarGhodke <ghodkeonkar84@gmail.com>
2026-01-21 14:48:32 +0530

    Fdone

--- src/admin/components/Footer.jsx
+++ src/admin/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
   return ( 
   <div className="sidebar-footer">
-    <button class="bt"><i class="fa-solid fa-right-from-bracket"></i>Logouts
+    <button class="bt"><i class="fa-solid fa-right-from-bracket"></i>Logout
     </button>
     </div>
   );
