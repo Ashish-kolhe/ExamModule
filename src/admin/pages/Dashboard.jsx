3e9294813e51fb11c52a0d7481930ab14d625a17
sahilbhatti292005-cloud <sahilbhatti292005@gmail.com>
2026-01-23 15:42:17 +0530

    AdminLoginPage

--- src/admin/pages/Dashboard.jsx
+++ src/admin/pages/Dashboard.jsx
@@ -1,9 +1,19 @@
 import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
+
 import { Sidebar } from "../components/Sidebar";
 import Main from "../components/Main";
 import { BottomNav } from "../components/BottomNav";
 
 export const Dashboard = () => {
+
+  // 🔐 AUTH CHECK (THIS IS THE PROTECTION)
+  const isAdmin = localStorage.getItem("adminAuth");
+
+  if (isAdmin !== "true") {
+    return <Navigate to="/admin/login" />;
+  }
+
   const [activePage, setActivePage] = useState("overview");
 
   return (
